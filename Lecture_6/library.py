bb75f859a15bca983d42bb983172291f2f1d01d8
Jatin Katyal <jatin.katyal13@gmail.com>
2018-09-15 18:22:59 +0530

    Lecture 6 added

---
+++ Lecture_6/library.py
@@ -0,0 +1,13 @@
+# this adds the two numbers
+def add(a: int, b: int) -> int:
+	''' this function
+	adds two numbers '''
+	return a+b
+
+def sub(a, b):
+	return a-b
+
+print(__name__)
+
+if __name__ == "__main__":
+	print(add(5, 6))
